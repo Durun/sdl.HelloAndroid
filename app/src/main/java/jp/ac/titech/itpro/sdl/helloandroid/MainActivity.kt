327cd3ca65d140870261fcfe5cece51d8444b7b7
Durun <negiigainuki@gmail.com>
2020-05-08 18:25:17 +0900

    feature: greet

--- app/src/main/java/jp/ac/titech/itpro/sdl/helloandroid/MainActivity.kt
+++ app/src/main/java/jp/ac/titech/itpro/sdl/helloandroid/MainActivity.kt
@@ -2,11 +2,19 @@ package jp.ac.titech.itpro.sdl.helloandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun greet(v: View) {
+        val name = input_name.text.trim()
+        if (name.isNotEmpty()) {
+            output_view.text = "Hello $name!\nNice to see you!"
+        }
     }
 }
